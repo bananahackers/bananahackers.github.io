a4a0a9cad6c106f61541bab64c43d74ef3fb5f69
Farooq Karimi Zadeh <farooqkz@hashbang.sh>
2020-10-29 08:01:02 +0100

    No need to these anymore

--- test_screensharing.js
+++
@@ -1,5 +0,0 @@
-var video = document.querySelector("video");
-navigator.mediaDevices.getUserMedia({video: {mediaSource: "screen"}}).then(stream => {
-  video.srcObject = stream;
-  video.play();
-});
